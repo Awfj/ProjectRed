280bbd785ee49f9bc6339ce8385bb3be715436e0
Alexey Kohanovsky <awfj837@gmail.com>
2022-10-14 11:35:52 +0300

    add classes: Character and MovingObject

---
+++ Physics/Character.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRed
+{
+    internal class Character : MovingObject
+    {
+        protected bool[] mInputs;
+        protected bool[] mPrevInputs;
+
+        public CharacterState mCurrentState = CharacterState.Stand;
+        public float mJumpSpeed;
+        public float mWalkSpeed;
+
+        protected bool Released(KeyInput key)
+        {
+            return (!mInputs[(int)key] && mPrevInputs[(int)key]);
+        }
+
+        protected bool KeyState(KeyInput key)
+        {
+            return mInputs[(int)key];
+        }
+
+        protected bool Pressed(KeyInput key)
+        {
+            return (mInputs[(int)key] && !mPrevInputs[(int)key]);
+        }
+
+        public enum KeyInput
+        {
+            GoLeft = 0,
+            GoRight,
+            GoDown,
+            Jump,
+            Count
+        }
+
+        public enum CharacterState
+        {
+            Stand,
+            Sit,
+            Walk,
+            Jump,
+        }
+    }
+}
---
+++ Physics/MovingObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static ProjectRed.Physics;
+
+namespace ProjectRed
+{
+    public class MovingObject
+    {
+        public Vector2 mOldPosition;
+        public Vector2 mPosition;
+
+        public Vector2 mOldSpeed;
+        public Vector2 mSpeed;
+
+        public Vector2 mScale;
+
+        public AABB mAABB;
+        public Vector2 mAABBOffset;
+
+        public bool mPushedRightWall;
+        public bool mPushesRightWall;
+        
+        public bool mPushedLeftWall;
+        public bool mPushesLeftWall;
+
+        public bool mWasOnGround;
+        public bool mOnGround;
+
+        public void UpdatePhysics()
+        {
+            mOldPosition = mPosition;
+            mOldSpeed = mSpeed;
+
+            mWasOnGround = mOnGround;
+            mPushedRightWall = mPushesRightWall;
+            mPushedLeftWall = mPushesLeftWall;
+
+            mPosition += mSpeed * Time.deltaTIme;
+
+            if (mPosition.Y < 0.0f)
+            {
+                mPosition.Y = 0.0f;
+                mOnGround = true;
+            }
+            else mOnGround = false;
+
+            mAABB.center = mPosition + mAABBOffset;
+        }
+    }
+}
