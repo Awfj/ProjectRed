fe2729327bba8146c8544f2419fd5b61d2c93ba6
Alexey Kohanovsky <awfj837@gmail.com>
2022-10-29 16:56:55 +0300

    add images: background, ground

--- Game1.cs
+++ Game1.cs
@@ -2,7 +2,8 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-//using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
 
 namespace ProjectRed
 {
@@ -10,26 +11,48 @@ namespace ProjectRed
     {
         Song song;
 
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        Ground ground, ground2, ground3, kutular1, boru1, boru2, boru4, boru5, kutular2, kutular3, boru3,
+                kutular4, kutular5, kutular6, kutular7, kutular8, miniboru1;
+        List<Ground> currentGroundList;
+
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        Texture2D background;
+        Vector2 bgposition = new Vector2(0, 0);
+
+        TimeSpan x = new TimeSpan(0, 0, 0);
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+
+            graphics.PreferredBackBufferWidth = 1366;
+            graphics.PreferredBackBufferHeight = 768;
+            graphics.IsFullScreen = true;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            ground = new Ground(Content, "aa", 0, 648);
+            ground2 = new Ground(Content, "aa", 1280, 648);
+            ground3 = new Ground(Content, "aa", 2560, 648);
+
+            currentGroundList = new List<Ground>();
+            currentGroundList.Add(ground);
+            currentGroundList.Add(ground2);
+            currentGroundList.Add(ground3);
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            background = Content.Load<Texture2D>("image_background");
 
             song = Content.Load<Song>("music_background");
             MediaPlayer.IsRepeating = true;
@@ -46,15 +69,32 @@ namespace ProjectRed
 
             // TODO: Add your update logic here
 
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Pink);
+            spriteBatch.Begin();
 
-            // TODO: Add your drawing code here
+            //Console.WriteLine(gameTime.ElapsedGameTime);
+            x += gameTime.ElapsedGameTime;
+            //Console.WriteLine(x.Milliseconds);
+            if (x.Milliseconds == 100)
+            {
+                //mario.animate();
+                x = gameTime.ElapsedGameTime;
+            }
 
+            spriteBatch.Draw(background, bgposition, Color.White);
+
+            foreach (var gnd in currentGroundList)
+            {
+                spriteBatch.Draw(gnd.texture, gnd.position, Color.White);
+            }
+
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ Ground.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRed
+{
+    internal class Ground
+    {
+        public ContentManager content;
+        public Texture2D texture;
+        public Vector2 position;
+        public int width, height;
+        public Rectangle gnd;
+
+        public Ground(ContentManager content, string tex, int x, int y)
+        {
+            //this.player = player;
+            this.content = content;
+            texture = content.Load<Texture2D>(tex);
+            width = texture.Width;
+            height = texture.Height;
+            position = new Vector2(x, y);
+            gnd = new Rectangle((int)position.X, (int)position.Y, width, height);
+
+        }
+    }
+}
